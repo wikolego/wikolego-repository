b7ca5733b99e95951c82fe20e024bb23e656ce18
wikolego <wikolego@wp.pl>
2025-04-28 17:43:12 +0200

    Added .ipynb and modified .py tensorflow_nn files

--- ai-path/ml-next-step/src/tensorflow_nn.py
+++ ai-path/ml-next-step/src/tensorflow_nn.py
@@ -1,8 +1,11 @@
 # Import required libraries and packages
+import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
 
 import config
+
+print(tf.__version__)
 
 # Load MNIST data
 dataframe = pd.read_csv(config.NEW_DATA_PATH)
@@ -13,8 +16,8 @@ df = dataframe.drop(columns=["id"])
 df_train = df.loc[df["kfold"] != 1, :]
 df_test = df.loc[df["kfold"] == 1, :]
 
-df_train.drop(columns=["kfold"])
-df_test.drop(columns=["kfold"])
+df_train = df_train.drop(columns=["kfold"])
+df_test = df_test.drop(columns=["kfold"])
 
 # Initialize model
 # model = tf.keras.models.Sequential([
@@ -25,8 +28,10 @@ df_test.drop(columns=["kfold"])
 # ])
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(28 * 28), # input layer
-    tf.keras.layers.Dense(128, activation='relu'),
+    # tf.keras.Input(shape=(None, None, 28 * 28)), # input layer
+    tf.keras.layers.Dense(64, activation="relu", input_shape=(28 * 28, )), # input layer
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(64, activation="relu"),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(10) # output layer
 ])
@@ -38,13 +43,22 @@ model.compile(
     metrics=["accuracy"]
 )
 
+# X_data = df_train.drop(columns=["label"])
+
+# print(len(X_data.columns))
+
+# exit()
+
 # Fit model
 result = model.fit(
-    df_train.loc[:, df.columns != 'label'],
-    df_train.loc[:, 'label'],
-    epochs=100
+    df_train.drop(columns=["label"]).values,
+    df_train.loc[:, "label"].values,
+    epochs=10
 )
 
 # Predict and print predictions
-predictions = max(result.history['accuracy'])
+predictions = max(result.history["accuracy"])
 print(predictions)
+
+plt.plot(result.history["accuracy"])
+plt.show()
