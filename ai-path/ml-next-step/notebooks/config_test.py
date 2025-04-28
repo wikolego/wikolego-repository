b37b3cf96bd8bc53b4f55180c328524d70ca888e
anka335 <joanna.zubik@uczen.zsk.poznan.pl>
2025-04-28 14:03:22 +0200

    PyTorch

---
+++ ai-path/ml-next-step/notebooks/config_test.py
@@ -0,0 +1,8 @@
+DOWNLOADED_IMAGES_PATH = '../data/t10k-images.idx3-ubyte'
+DOWNLOADED_LABELS_PATH = '../data/t10k-labels.idx1-ubyte'
+
+BASIC_DATA_PATH = "../data/train.csv"
+NEW_DATA_PATH = "../data/train_no_ids.csv"
+
+RANDOMIZE_DATA = True
+FOLDS_COUNT = 5
