cee7cab77eea44539497a98aa2bfe2b638a46b73
wikolego <wikolego@wp.pl>
2025-05-02 13:01:36 +0200

    Added folder for presentation. Created classic_methods notebook and different files in that folder

---
+++ ai-path/ml-next-step/presentation/bibliography.md
@@ -0,0 +1,3 @@
+# Bibliography
+
+- https://towardsdatascience.com/how-to-cross-validation-with-time-series-data-9802a06272c6/
---
+++ ai-path/ml-next-step/presentation/config.py
@@ -0,0 +1,15 @@
+# Paths config
+DOWNLOADED_IMAGES_PATH = '../data/t10k-images.idx3-ubyte'
+DOWNLOADED_LABELS_PATH = '../data/t10k-labels.idx1-ubyte'
+
+BASIC_DATA_PATH = "../data/train.csv"
+NEW_DATA_PATH = "../data/train_new.csv"
+
+# Format data config
+RANDOMIZE_DATA = True
+
+# General project settings
+FOLDS_CNT = 5
+
+# Neutral networks settings
+EPOCHS_CNT = 20
