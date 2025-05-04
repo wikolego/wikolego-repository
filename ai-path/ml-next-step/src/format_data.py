50c80bf86dbecf71bd938f401a5e6e78d11a24ca
wikolego <wikolego@wp.pl>
2025-05-05 01:26:15 +0200

    Changed format_data.py to delete id label and modified train_new.csv. Modified tensorflow.ipynb by adding comments, looping through folds and displaying results. Changed a little bit other files.

--- ai-path/ml-next-step/src/format_data.py
+++ ai-path/ml-next-step/src/format_data.py
@@ -7,18 +7,21 @@ import config
 # Load MNIST data
 df = pd.read_csv(config.BASIC_DATA_PATH)
 
-# If needed, randomize data
-if config.RANDOMIZE_DATA == True:
-    df = df.sample(frac=1).reset_index(drop=True)
-    df["id"] = df.index
+df = df.drop(columns=["id"])
+
+# Initialize kfold column
+df["kfold"] = -1
 
 # Make k-folds
-folds_model = model_selection.StratifiedKFold(n_splits=config.FOLDS_CNT)
+folds_model = model_selection.StratifiedKFold(
+    n_splits=config.FOLDS_CNT,
+    shuffle=config.RANDOMIZE_DATA
+)
 
-for index, (rest, selected) in enumerate(folds_model.split(X=df, y=df["label"])):
+for fold, (rest, selected) in enumerate(folds_model.split(X=df, y=df["label"])):
     # print(f"{x}. {fir} - {sec}")
     # print(len(fir), len(sec))
-    df.loc[selected, "kfold"] = index
+    df.loc[selected, "kfold"] = fold
 
 # save MNIST data
 df.to_csv(config.NEW_DATA_PATH, index=False)
