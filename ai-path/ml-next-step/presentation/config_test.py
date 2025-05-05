8bea679cf13fbc4b535a8d03a4828d7dd769153e
anka335 <joanna.zubik@uczen.zsk.poznan.pl>
2025-05-05 15:05:00 +0200

    zmiana nazwy zmiennej, config_test

---
+++ ai-path/ml-next-step/presentation/config_test.py
@@ -0,0 +1,15 @@
+# Paths config
+DOWNLOADED_IMAGES_PATH = '../data/t10k-images.idx3-ubyte'
+DOWNLOADED_LABELS_PATH = '../data/t10k-labels.idx1-ubyte'
+
+BASIC_DATA_PATH = "../data/train.csv"
+NEW_DATA_PATH = "../data/train_no_ids.csv"
+
+# Format data config
+RANDOMIZE_DATA = True
+
+# General project settings
+FOLDS_COUNT = 5
+
+# Neutral networks settings
+EPOCHS_CNT = 20
